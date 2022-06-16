0e05b4e015dd59685288bf900dc6d7130379a8fb
jushetcon <b.e.s.dark@gmail.com>
2022-06-16 11:45:53 +0300

    new learnjs tasks

--- learnjs/number.js
+++ learnjs/number.js
@@ -9,3 +9,35 @@ alert(num1 + num2)
 
 // Как правильно округлить 6.35?
 console.log(Math.round(6.35 * 10) / 10) // 6.4
+
+// Создайте функцию readNumber, которая будет запрашивать ввод числового значения до тех пор, пока посетитель его не введёт
+
+function readNumber() {
+	let number
+
+	do {
+		number = prompt('Число?', 0)
+	} while (!isFinite(number))
+
+	if (number == '' || number == null) return null
+	return +number
+}
+
+alert(`Число ${readNumber()}`)
+
+// Напишите функцию random(min, max), которая генерирует случайное число с плавающей точкой от min до max (но не включая max).
+
+function random(min, max) {
+	return min + Math.random() * (max - min)
+}
+
+console.log(random(1, 6))
+
+// Напишите функцию randomInteger(min, max), которая генерирует случайное целое (integer) число от min до max (включительно).
+
+function randomNumber(min, max) {
+	let num = min + Math.random() * (max - min)
+	return Math.round(num)
+}
+
+alert(randomNumber(1, 3))
---
+++ learnjs/string.js
@@ -0,0 +1,35 @@
+//Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом.
+
+function ucFirst(str) {
+	if (!str) return str
+
+	return str[0].toUpperCase() + str.slice(1)
+}
+
+alert(ucFirst('дмитрий'))
+
+// Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX', а иначе false.
+
+function checkSpam(str) {
+	let lowerStr = str.toLowerCase()
+
+	return (
+		lowerStr.includes('viagra') ||
+		lowerStr.includes('xxx') ||
+		lowerStr.includes('innocent rabbit')
+	)
+}
+
+// Создайте функцию truncate(str, maxlength), которая проверяет длину строки str и, если она превосходит maxlength, заменяет конец str на "…", так, чтобы её длина стала равна maxlength.
+
+function truncate(str, maxlength) {
+	return str.length > maxlength ? str.slice(0, maxlength - 1) + '…' : str
+}
+
+//Есть стоимость в виде строки "$120". То есть сначала идёт знак валюты, а затем – число.
+
+// Создайте функцию extractCurrencyValue(str), которая будет из такой строки выделять числовое значение и возвращать его.
+
+function extractCurrencyValue(str) {
+	return +str.slice(1)
+}
